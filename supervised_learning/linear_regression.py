86b435e1f0177751e5e7aeb1e9d39d3386691b07
Sh1ntaro05 <shintarokimura3@gmail.com>
2026-05-18 19:52:43 +0900

    Wrote summary function for linear regression.py

--- supervised_learning/linear_regression.py
+++ supervised_learning/linear_regression.py
@@ -1,24 +1,54 @@
 import numpy as np
+from scipy import stats
 
 class LinearRegression:
     def __init__(self):
+        self.data_n = None
+        self.p = None
         self.beta = None 
+        self.rss = None
+        self.tss = None
+        self.beta_varmatrix = None
+        self.stderr = None
+        self.tscores = None 
+        self.pvals = None
+        
+        pass
 
     #Recieves: Training data (the X and the y)
     #Does: Calculates important statistics including the beta, variance of beta, etc.
     #Returns: Nothing
     def fit(self,X,y):
-        n = X.shape[0]
-        X_design = np.empty((n,X.shape[1]+1))
+        self.data_n = X.shape[0]
+        self.p = X.shape[1]
+
+        X_design = np.empty((self.data_n,X.shape[1]+1))
         X_design[:,0] = 1.0
         X_design[:,1:] = X
         X_T = np.transpose(X_design)
-        self.beta = np.linalg.inv(X_T @ X_design) @ (X_T @ y)
+        X_T_X_inv = np.linalg.inv(X_T @ X_design)
 
+        self.beta = X_T_X_inv @ (X_T @ y)
+        self.beta = self.beta.reshape(-1,1)
+
+        y_hat = X_design @ self.beta
+        
+        self.rss = np.sum((y - y_hat) ** 2)
+        self.tss = np.sum((y - np.mean(y)) ** 2)
+        df = self.data_n - self.p - 1
+        sigma2 = self.rss / df
+        self.beta_varmatrix = X_T_X_inv * sigma2
+        self.stderr = np.sqrt(np.diagonal(self.beta_varmatrix)).reshape(-1,1)
+        self.tscores = self.beta / self.stderr
+        self.pvals = stats.t.sf(np.abs(self.tscores),df=df) * 2
+        
     #Recieves: Data to predict y value
     #Does: Using the precalculated beta vector, calculates the estimation of y
     #Returns: The predicted y value
     def predict(self, X):
+        if self.beta is None:
+            print("First run the fit function to calculate coefficient estimates.")
+
         X = np.asarray(X)
 
         if X.ndim == 1:
@@ -31,7 +61,15 @@ class LinearRegression:
 
         return X_design @ self.beta
     
+    #Recieves: Nothing
+    #Does: Prints out a summary of significant statistics
+    #Returns: Nothing
     def summary(self):
+        print(f"beta_hat: {self.beta.T}")
+        print(f"std. error: {self.stderr.T}")
+        print(f"t score: {self.tscores.T}")
+        print(f"P values: {self.pvals.T}")
+
         pass
 
 
@@ -39,6 +77,7 @@ def main():
     rng = np.random.default_rng(seed=1)
     p = 5
     n = 10
+    noise_var = 1.0
 
     beta = 10 * rng.random((p+1,1))
     
@@ -47,7 +86,7 @@ def main():
     X_design[:, 0] = 1.0
     X_design[:, 1:] = X    
 
-    noise = rng.normal(0,1.0,(n,1))
+    noise = rng.normal(0,noise_var,(n,1))
 
     y = X_design @ beta + noise
 
@@ -55,9 +94,8 @@ def main():
     lin_regressor.fit(X,y)
 
     beta_hat = lin_regressor.beta
-    print(beta.T)
-    print(beta_hat.T)
-
+    print(f"True beta: {beta.T}")
+    lin_regressor.summary()
 
     pass
 
