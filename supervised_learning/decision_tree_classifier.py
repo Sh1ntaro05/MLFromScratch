da5c50d9751584987b7bf3727f8276bb8c9a5bc0
Sh1ntaro05 <shintarokimura3@gmail.com>
2026-05-20 12:32:38 +0900

    Created py file for decision tree classification; will do bagged tree and random forest later

---
+++ supervised_learning/decision_tree_classifier.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+class Node:
+    def __init__():
+
+
+        pass
+
+
+
+
+
+class DecisionTreeClassifier:
+    def __init__():
+
+
+        pass
+
+    def fit():
+
+
+        pass
+
+    def predict():
+
+        
+        pass 
+
+
+def main():
+
+
+    pass
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
